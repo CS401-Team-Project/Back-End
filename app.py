08e33cc72389ebcc2ae82be3b97f2b1fa1b1de6d
Andrei Cozma <me@andreicozma.com>
2022-02-27 20:43:25 -0500

    Setup entrypoint for setting up backend db and use cleaned up environment variables

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import datetime
 import os
 
-from flask import Flask, Response, request, jsonify
+from flask import Flask, request, jsonify
 from flask_mongoengine import MongoEngine
+
 from models import *
+
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
-    'host': os.environ['MONGODB_HOST'],
-    'username': os.environ['MONGODB_USERNAME'],
-    'password': os.environ['MONGODB_PASSWORD'],
+    'host': os.environ['MONGO_HOST'],
+    'username': os.environ['API_USERNAME'],
+    'password': os.environ['API_PASSWORD'],
     'authSource': 'smart-ledger',
     'db': 'smart-ledger'
 }
@@ -19,6 +20,7 @@ db.init_app(app)
 
 @app.route("/person", methods=['GET'])
 def get_person():
+    print(f"ROUTE get_person(): {request}")
     email = request.args.get('email')
     person = Person.objects(email=email).first()
     return jsonify(person), 200
@@ -26,10 +28,12 @@ def get_person():
 
 @app.route("/group", methods=['GET'])
 def get_group():
+    print(f"ROUTE get_group: {request}")
     g_id = request.args.get('id')
     group = Group.objects(id=g_id).first()
     return jsonify(group), 200
 
 
 if __name__ == "__main__":
+    # TODO: Change to production debug False
     app.run(debug=True, port=5000)
--- scripts/fill_db.py
+++ scripts/fill_db.py
@@ -1,41 +1,42 @@
 # general utils
 import json
-import os
-import sys
-import string
-
 # for the randomness
+import os
 import random
-import names
-import randomname
-import numpy as np
+import string
+import sys
 
+import names
+import numpy as np
+# add the models package to the path
+import path
+import randomname
 # flask imports
 from flask import Flask
 from flask_mongoengine import MongoEngine
 
-# add the models package to the path
-import path
 directory = path.Path(__file__).abspath()
 sys.path.append(directory.parent.parent)
 from models import *
 
+if len(sys.argv) != 3:
+    print('Usage: python3 fill_db.py [admin-username] [admin-password]')
+    sys.exit()
+
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
-    'host': os.environ['MONGODB_HOST'],
-    'username': os.environ['MONGODB_USERNAME'],
-    'password': os.environ['MONGODB_PASSWORD'],
+    'host': os.environ['MONGO_HOST'],
+    'username': os.environ['API_USERNAME'],
+    'password': os.environ['API_PASSWORD'],
     'authSource': 'smart-ledger',
     'db': 'smart-ledger',
-
 }
 
 db = MongoEngine()
 db.init_app(app)
 
-
 # get config for data generation
-config = 'db_scripts/example_data/data_gen_config.json'
+config = 'scripts/example_data/data_gen_config.json'
 if len(sys.argv) == 2:
     config = sys.argv[1]
 with open(config, 'r') as f:
@@ -50,7 +51,8 @@ num_groups = int(config['num_groups'])
 min_group_size, max_group_size = int(config['min_group_size']), int(config['max_group_size'])
 min_transactions, max_transactions = int(config['min_transactions']), int(config['max_transactions'])
 min_item_quantity, max_item_quantity = int(config['min_transactions']), int(config['max_item_quantity'])
-min_item_per_transac_item, max_item_per_transac_item = int(config['min_item_per_transac_item']), int(config['max_item_per_transac_item'])
+min_item_per_transac_item, max_item_per_transac_item = int(config['min_item_per_transac_item']), int(
+    config['max_item_per_transac_item'])
 min_item_price, max_item_price = float(config['min_item_price']), float(config['max_item_price'])
 email_usernames = config['email_usernames'] if 'email_usernames' in config else None
 name_list = config['names'] if 'names' in config else None
@@ -68,7 +70,7 @@ if name_list is not None:
         name_list = json.load(f)
 
 # fix some values if needed
-dec_if_equal = lambda x, y, dec=1: x-dec if x == y else x
+dec_if_equal = lambda x, y, dec=1: x - dec if x == y else x
 min_group_size = dec_if_equal(min_group_size, max_group_size)
 min_group_size = dec_if_equal(min_group_size, max_group_size)
 min_email_len = dec_if_equal(min_email_len, max_email_len)
@@ -108,8 +110,10 @@ def get_one_random_domain(domains):
 # add people to database
 p_ids = []
 for i in range(num_users):
-    first_name, last_name = (names.get_first_name(), names.get_last_name()) if name_list is None else (name_list[i]['first_name'], name_list[i]['last_name'])
-    username = f'{first_name}_{last_name}_{np.random.randint(0,1000):0>4}' if email_usernames is None else email_usernames[i]
+    first_name, last_name = (names.get_first_name(), names.get_last_name()) if name_list is None else (
+        name_list[i]['first_name'], name_list[i]['last_name'])
+    username = f'{first_name}_{last_name}_{np.random.randint(0, 1000):0>4}' if email_usernames is None else \
+        email_usernames[i]
     p = {
         "first_name": first_name,
         "last_name": last_name,
@@ -134,7 +138,7 @@ people = Person.objects()
 groups = Group.objects()
 num_groups = len(people)
 num_people = len(groups)
-people_indices = np.arange(0,num_people)
+people_indices = np.arange(0, num_people)
 group_people = {}
 for g in groups:
     people_to_add = np.random.choice(people_indices, np.random.randint(min_group_size, max_group_size))
@@ -173,7 +177,7 @@ for g_id in g_ids:
 
 # randomly add items to transactions
 def random_float(low, high):
-    return np.random.random()*(high-low) + low
+    return np.random.random() * (high - low) + low
 
 
 # add random number of items into a transaction
@@ -207,12 +211,11 @@ for t_id in t_ids:
             'item_id': i_id,
             'person': group_people[g_id][p_id_i],
             'quantity': quantity,
-            'item_cost': item_cost*quantity
+            'item_cost': item_cost * quantity
         }
         transac_item = TransactionItem(**transac_item)
-        total_price += item_cost*quantity
+        total_price += item_cost * quantity
         t.items.append(transac_item)
         t.save()
     t.total_price = total_price
     t.save()
-

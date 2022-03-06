2f2f65001638e9c25dc276cc90160aab912b29de
beanh0le <echavez2@vols.utk.edu>
2022-03-05 21:36:47 -0500

    register_user() and check_if_registered
    not done but probably almost done adding function to check if user is registered and registration of new user

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import datetime
 import os
 
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
 from flask import Flask, Response, request, jsonify
 from flask_mongoengine import MongoEngine
 from models import *
@@ -16,6 +19,8 @@ app.config['MONGODB_SETTINGS'] = {
 db = MongoEngine()
 db.init_app(app)
 
+# dont hardcode this. Will fix at later date
+CLIENT_ID = '996982688294-rqhcr4d5b6m3vk0e3ur2no457iqql2u5.apps.googleusercontent.com' 
 
 @app.route("/person", methods=['GET'])
 def get_person():
@@ -30,6 +35,47 @@ def get_group():
     group = Group.objects(id=g_id).first()
     return jsonify(group), 200
 
+
+def check_if_registered():
+    token = request.form["idtoken"]
+    try:
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+
+        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            raise ValueError('Wrong issuer.')
+
+        # ID token is valid. Get the user's Google Account ID from the decoded token.
+        userid = idinfo['sub']
+        num_results = Person.objects(sub=userid).count()
+        
+        # User in database if 1. Not in database if 0
+        return num_results
+
+    except ValueError:
+        # Invalid token
+        return -1
+        
+
+def register_user():
+    token = request.form["idtoken"]
+    try:
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+
+        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            raise ValueError('Wrong issuer.')
+
+        # ID token is valid. Get the user's Google Account ID from the decoded token.
+        userid = idinfo['sub']
+        f_name = idinfo['given_name']
+        l_name = idinfo['family_name']
+        em = idinfo['email']
+        exp = datetime.datetime.fromtimestamp(idinfo['exp'])
+        p = Person(first_name=f_name, last_name=l_name, email=em, sub=userid, token_latest=token, token_exp = exp).save()
+        return 1
+
+    except ValueError:
+        # Invalid token
+        return -1
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- models/Person.py
+++ models/Person.py
@@ -8,6 +8,9 @@ class Person(Document):
     email = EmailField(max_length=100, required=True)
     groups = ListField(default=[])
     date_joined = DateTimeField(default=datetime.datetime.utcnow)
+    sub = StringField(max_length=255, unique=True)
+    token_latest # Not sure what kind of field this should be or if this is what i should store
+    token_exp = DateTimeField
     # TODO - Fields to add
     #  - API Key ( will have to change regularly )
     #  - API Key Last Generated ( for google auth and email auth )

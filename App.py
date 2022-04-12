857e95b045ca3470c1b73f24bd776b8501e2df1b
beanh0le <echavez2@vols.utk.edu>
2022-04-11 20:59:51 -0400

    fixed some instances of incorrect naming
    there may still be other naming mistakes

--- App.py
+++ App.py
@@ -317,7 +317,7 @@ def delete_profile(person):
     # TODO - we need to figure out a policy to show users past transactions after their account has been deleted
     for group in person.groups:
         try:
-            group.people.remove(person.sub)
+            group.members.remove(person.sub)
         except Exception:
             pass
 
@@ -560,7 +560,7 @@ def join_group(person):
         group.invites.remove(person.email)
 
     # add person to group
-    group.people.append(person.sub)
+    group.members.append(person.sub)
     group.updated = datetime.datetime.utcnow()
 
     # save group
@@ -655,7 +655,7 @@ def remove_member(person):
     # if the user is trying to delete another user in the group
     if sub is not None:
         # check if authorized to remove member
-        if (group.settings.only_admin_remove_user and group.admin != person.sub) or sub == group.admin:
+        if (group.restricted.permissions.only_admin_remove_user and group.admin != person.sub) or sub == group.admin:
             return jsonify({'msg': 'Token is unauthorized or group does not exist.'}), 404
 
     else:
@@ -663,11 +663,11 @@ def remove_member(person):
         sub = person.sub
 
     # check if the given sub is not in group
-    if sub not in group.people:
+    if sub not in group.members:
         return jsonify({'msg': 'User is not a member of the group.'}), 409
 
     # remove the person from the group
-    group.people.remove(sub)
+    group.members.remove(sub)
 
     # save the group
     group.updated = datetime.datetime.utcnow()
@@ -766,7 +766,7 @@ def create_transaction(person):
     group = Group.objects.get(id=group_id)
 
     # make sure the user belongs to the group
-    if person.sub not in group.people:
+    if person.sub not in group.members:
         return jsonify({'msg': 'Token is unauthorized.'}), 404
 
     # create the transaction
@@ -821,12 +821,12 @@ def update_transaction(person):
     group = Group.objects.get(id=group_id)
 
     # make sure the user belongs to the group
-    if person.sub not in group.people:
+    if person.sub not in group.members:
         return jsonify({'msg': 'Token is unauthorized.'}), 404
 
     # make sure user is authorized
-    if not (group.settings.admin_overrule_transaction and group.admin == person.sub):
-        if not (group.settings.only_owner_modify_transaction and transaction.created_by == person.sub):
+    if not (group.restricted.permissions.admin_overrule_transaction and group.admin == person.sub):
+        if not (group.restricted.permissions.only_owner_modify_transaction and transaction.created_by == person.sub):
             return jsonify({'msg': 'Token is unauthorized.'}), 404
 
     # update the transaction
@@ -891,13 +891,13 @@ def delete_transaction(person):
     group = Group.objects.get(id=group_id)
 
     # make sure the user belongs to the group
-    if person.sub not in group.people:
+    if person.sub not in group.members:
         return jsonify({'msg': 'Token is unauthorized.'}), 404
 
     # make sure user is authorized
-    if not (group.settings.admin_overrule_delete_transaction and group.admin == person.sub):
-        if not group.settings.user_delete_transaction:
-            if not (group.settings.only_owner_delete_transaction and transaction.created_by == person.sub):
+    if not (group.restricted.permissions.admin_overrule_delete_transaction and group.admin == person.sub):
+        if not group.restricted.permissions.user_delete_transaction:
+            if not (group.restricted.permissions.only_owner_delete_transaction and transaction.created_by == person.sub):
                 return jsonify({'msg': 'Token is unauthorized.'}), 404
 
     # check if transaction is the group
@@ -1009,7 +1009,7 @@ def _add_item_to_transaction(person, transaction, quantity, person_id, name, des
     sub = person_id if person_id is not None else person.sub
 
     # make sure the user belongs to the group
-    if person.sub not in group.people or sub not in group.people:
+    if person.sub not in group.members or sub not in group.members:
         raise Exception('Person does not belong to group')
 
     # check quantity for proper value
